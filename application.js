8ff6e36892ed1f5fe11dba1a521a679ef402c584
Jais Cheema <jais@trikeapps.com>
2013-08-23 09:06:43 +1000

    Ad proper styles and a basic service for fetching latest albums

--- application.js
+++ application.js
@@ -1,6 +1,42 @@
-function PhoneListCtrl($scope) {
-  $scope.phones = [
-    {name: "iPhone 16GB", snippet: "Awesome phone" },
-    {name: "Galaxy S4", snippet: "Shit phone with awesome camera and screen" }
-  ];
-}
+var app = angular.module("perann", []);
+
+
+app.factory('dataFactory', ['$http', function($http) {
+  var urlBase = 'http://squirrel.jaischeema.com/v1/';
+  var dataFactory = {};
+
+  dataFactory.getLatestAlbums = function () {
+    return $http.get(urlBase + "latest_albums");
+  };
+
+  dataFactory.getAlbum = function(albumID) {
+    return $http.get(urlBase + "album?id=" + albumID);
+  };
+  return dataFactory;
+}]);
+
+app.directive('album', function(){
+  return {
+    restrict: "E",
+    template: "<div>hello</div>"
+  };
+});
+
+app.controller("MainCtrl", ['$scope', 'dataFactory', function($scope, dataFactory){
+  $scope.latest_albums = [];
+  getLatestAlbums();
+  function getLatestAlbums() {
+    dataFactory.getLatestAlbums().success(function(albums){
+      $scope.latest_albums = albums.albums;
+    })
+  }
+
+  function showAlbum(album) {
+    console.log(album);
+  };
+
+  $scope.$on('showAlbum', function(){
+    console.log("yayyy");
+  });
+
+}]);
