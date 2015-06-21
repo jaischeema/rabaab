f0b15ce8da3ef02f72990030d3612ecb5118b7ba
Jais Cheema <jaischeema@gmail.com>
2015-06-21 15:33:08 +1000

    Use album list and the spinner in the latest albums page

--- app/scripts/components/latest_albums.js
+++ app/scripts/components/latest_albums.js
@@ -1,25 +1,37 @@
 import React from 'react';
+import Spinner from './spinner';
+import AlbumList from './album_list';
 import { Link } from 'react-router';
 
 export default class extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {albums: []};
+    this.state = {albums: [], loading: true};
     var self = this;
     $.getJSON('http://squirrel.jaischeema.com/api/albums/latest', ((response) => {
-      this.setState({albums: response.albums});
+      this.setState({albums: response.albums, loading: false});
     }));
   }
 
   render() {
-    return (
-      <ul>
-        {this.state.albums.map((album) => { return this.renderAlbum(album) })}
-      </ul>
-    );
-  }
-
-  renderAlbum(album) {
-    return (<li><Link to={`albums/${album.resource_id}/${album.title}`}>{album.title}</Link></li>);
+    if(this.state.loading) {
+      return (
+        <Spinner
+          config={{
+            width: 12,
+            radius: 25,
+          }}
+        />
+      );
+    } else {
+      return (
+        <div className="content">
+          <div className="page-title">
+            <h1>Latest Albums</h1>
+          </div>
+          <AlbumList albums={this.state.albums} />
+        </div>
+      );
+    }
   }
 }
