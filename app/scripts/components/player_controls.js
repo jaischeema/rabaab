774900c80cc23c1e94dd1900ca591fc5d53404fa
Jais Cheema <jaischeema@gmail.com>
2015-06-21 15:34:10 +1000

    Use the proper icons and styling in the player controls

--- app/scripts/components/player_controls.js
+++ app/scripts/components/player_controls.js
@@ -5,7 +5,7 @@ import { PlaylistActions } from '../playlist';
 export default class extends React.Component {
   render() {
     return (
-      <div className="playerControls">
+      <div className="player-controls">
         {(this.props.song != null) ? this.renderPlayControls() : this.renderBlank()}
       </div>
     );
@@ -15,18 +15,36 @@ export default class extends React.Component {
     var divClass = this.props.loadingSong ? "controls loading" : "controls";
     return (
       <div className={divClass}>
-        <span onClick={this.onPreviousButtonClick}>Previous</span>
-        <span onClick={this.onPlayButtonClick.bind(this)}>Play</span>
-        <span onClick={this.onNextButtonClick}>Next</span>
-        <span>{this.props.song.title}</span>
-        <span>{this.props.song.artistTitle}</span>
-        <span>
-          <FormattedTime time={this.props.currentPosition} />
-          <span>/</span>
-          <FormattedTime time={this.props.totalDuration} />
-        </span>
+        <div className="button col-sm-2">
+          <i className="icon-control-rewind" onClick={this.onPreviousButtonClick} />
+          {this.renderPlayPauseButton()}
+          <i className="icon-control-forward" onClick={this.onNextButtonClick} />
+        </div>
+        <div className="timers col-sm-10">
+          <div className="progress-bar" />
+          <div className="loading-bar" />
+          <div className="info">
+            <span>{this.props.song.title}</span>
+            <span>{this.props.song.artistTitle}</span>
+          </div>
+          <div className="ticker">
+            <FormattedTime time={this.props.currentPosition} />
+            <span>/</span>
+            <FormattedTime time={this.props.totalDuration} />
+          </div>
+        </div>
       </div>
     );
+  }
+
+  renderPlayPauseButton() {
+    var iconClass;
+    if(this.props.paused) {
+      iconClass = "icon-control-play play-button";
+    } else {
+      iconClass = "icon-control-pause play-button";
+    }
+    return (<i className={iconClass} onClick={this.onPlayButtonClick.bind(this)} />);
   }
 
   onPlayButtonClick() {
